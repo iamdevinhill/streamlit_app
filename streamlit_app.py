9063f191f03b514d78b41d575acdc75dc5ee6814
Devin Hill <33045257+iamdevinhill@users.noreply.github.com>
2022-08-20 18:06:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,5 +67,6 @@ if st.button('Intercorrelation Heatmap'):
     with sns.axes_style("white"):
         f, ax = plt.subplots(figsize=(7, 5))
         ax = sns.heatmap(corr, mask=mask, vmax=1, square=True)
+    st.set_option('deprecation.showPyplotGlobalUse', False)
     st.pyplot()
-st.set_option('deprecation.showPyplotGlobalUse', False)
+
